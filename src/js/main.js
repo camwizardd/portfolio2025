b35ab75d6566c516a7b842242975d6a8111de5e2
camwizardd <camille.magnin@studentfr.ch>
2026-01-16 14:57:30 +0100

    version desktop des trucs + effets aimg curseur finit

--- src/js/main.js
+++ src/js/main.js
@@ -85,4 +85,51 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "rotation",
     inertia: true,
   });
+  Draggable.create(".project-new-desktop-icon", {
+    type: "rotation",
+    inertia: true,
+  });
+
+  //  SHOW IMG ON HOVER----------------------------------------------------------------------------------------------------------------------
+
+  gsap.set(".project-preview img.swipeimage, video.swipeimage", {
+    yPercent: -50,
+    xPercent: -50,
+  });
+
+  let firstEnter;
+
+  gsap.utils.toArray(".project-preview").forEach((el) => {
+    const image = el.querySelectorAll("img.swipeimage, video.swipeimage")[0],
+      setX = gsap.quickTo(image, "x", { duration: 0.4, ease: "power3" }),
+      setY = gsap.quickTo(image, "y", { duration: 0.4, ease: "power3" }),
+      align = (e) => {
+        if (firstEnter) {
+          setX(e.clientX, e.clientX);
+          setY(e.clientY, e.clientY);
+          firstEnter = false;
+        } else {
+          setX(e.clientX);
+          setY(e.clientY);
+        }
+      },
+      startFollow = () => document.addEventListener("mousemove", align),
+      stopFollow = () => document.removeEventListener("mousemove", align),
+      fade = gsap.to(image, {
+        autoAlpha: 1,
+        ease: "none",
+        paused: true,
+        duration: 0.1,
+        onReverseComplete: stopFollow,
+      });
+
+    el.addEventListener("mouseenter", (e) => {
+      firstEnter = true;
+      fade.play();
+      startFollow();
+      align(e);
+    });
+
+    el.addEventListener("mouseleave", () => fade.reverse());
+  });
 });
